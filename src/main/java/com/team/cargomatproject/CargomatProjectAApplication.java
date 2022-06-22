70d92adebbbef1f6749200d6cf75567f968a4584
Роман Орлов <roma_5712@mail.ru>
2022-06-22 23:43:04 +0500

    first commit

---
+++ src/main/java/com/team/cargomatproject/CargomatProjectAApplication.java
@@ -0,0 +1,13 @@
+package com.team.cargomatproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargomatProjectAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CargomatProjectAApplication.class);
+    }
+
+}
---
+++ src/test/java/com/team/cargomatproject/CargomatProjectAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.cargomatproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargomatProjectAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
